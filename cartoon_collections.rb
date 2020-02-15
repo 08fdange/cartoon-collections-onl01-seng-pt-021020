cc6a891a0d4ee6184d29df78c03a673cf1c39a9e
Frank  D'Angelo <08fdange@gmail.com>
2020-02-15 20:31:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    item.include?(cheese_types)
-      return item
-    
+   return item.include?(cheese_types)
   end
 end
